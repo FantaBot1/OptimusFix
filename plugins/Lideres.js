b056994f660bbda3b58e5539a1c4cbc1c08be848
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-03 19:13:19 -0500

    𝗧𝗲𝗮𝗺 𝗛𝘆𝗱𝗿𝗮 𝗕𝗼𝘁

---
+++ plugins/Lideres.js
@@ -0,0 +1,16 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.lideres);
+};
+handler.command = /^(Lideres|lideres)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.Lideres = `
+*Hola Si Deseas Hablarle A los Lideres Y Ceos Del Team Hydra Estos Son Sus Numeros :* 
+*1 :* +51 926 933 818 *Ceo Hydra*
+*2 :* +51 980 269 276 *Co Ceo Hydra*
+*3 :* +51 978 420 752 *Co Lider Hydra*
+*4 :* +54 9 291 513 9790 *Decana Hydra*
+`;
