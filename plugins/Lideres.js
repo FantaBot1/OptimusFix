afa8797aaa4abc641b40993f2c4c5e740df2a597
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-02-04 01:23:18 +0100

    Update Lideres.js

--- plugins/Lideres.js
+++ plugins/Lideres.js
@@ -7,8 +7,8 @@ handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
-global.Lideres = `
-*Hola Si Deseas Hablarle A los Lideres Y Ceos Del Team Hydra Estos Son Sus Numeros :* 
+global.Lideres = `*Hola Si Deseas Hablarle A los Lideres Y Ceos Del Team Hydra Estos Son Sus Numeros :* 
+
 *1 :* +51 926 933 818 *Ceo Hydra*
 *2 :* +51 980 269 276 *Co Ceo Hydra*
 *3 :* +51 978 420 752 *Co Lider Hydra*
